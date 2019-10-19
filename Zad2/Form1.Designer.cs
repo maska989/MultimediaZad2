b512d625831788ca14c6d51538a181e85a415423
Modzel <hootwheels989@gmail.com>
2019-10-19 21:40:56 +0200

    Wymagania - obsługa przycisków oraz ładowanie całego katalogu

--- Zad2/Form1.Designer.cs
+++ Zad2/Form1.Designer.cs
@@ -39,51 +39,60 @@
             this.ususnToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.zamknijToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.listView1 = new System.Windows.Forms.ListView();
+            this.Góra = new System.Windows.Forms.Button();
+            this.Dół = new System.Windows.Forms.Button();
+            this.Reset = new System.Windows.Forms.Button();
+            this.Wczytaj = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.RotPraw = new System.Windows.Forms.Button();
+            this.RotLew = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // Plus
             // 
-            this.Plus.Location = new System.Drawing.Point(713, 396);
+            this.Plus.Location = new System.Drawing.Point(700, 436);
             this.Plus.Name = "Plus";
             this.Plus.Size = new System.Drawing.Size(75, 42);
             this.Plus.TabIndex = 0;
             this.Plus.Text = "+";
             this.Plus.UseVisualStyleBackColor = true;
+            this.Plus.Click += new System.EventHandler(this.Plus_Click);
             // 
             // Minus
             // 
-            this.Minus.Location = new System.Drawing.Point(632, 396);
+            this.Minus.Location = new System.Drawing.Point(619, 436);
             this.Minus.Name = "Minus";
             this.Minus.Size = new System.Drawing.Size(75, 42);
             this.Minus.TabIndex = 1;
             this.Minus.Text = "-";
             this.Minus.UseVisualStyleBackColor = true;
+            this.Minus.Click += new System.EventHandler(this.Minus_Click);
             // 
             // Prawo
             // 
-            this.Prawo.Location = new System.Drawing.Point(351, 396);
+            this.Prawo.Location = new System.Drawing.Point(362, 437);
             this.Prawo.Name = "Prawo";
-            this.Prawo.Size = new System.Drawing.Size(75, 42);
+            this.Prawo.Size = new System.Drawing.Size(40, 40);
             this.Prawo.TabIndex = 2;
-            this.Prawo.Text = "->";
+            this.Prawo.Text = "→";
             this.Prawo.UseVisualStyleBackColor = true;
             // 
             // Lewo
             // 
-            this.Lewo.Location = new System.Drawing.Point(270, 396);
+            this.Lewo.Location = new System.Drawing.Point(270, 437);
             this.Lewo.Name = "Lewo";
-            this.Lewo.Size = new System.Drawing.Size(75, 42);
+            this.Lewo.Size = new System.Drawing.Size(40, 40);
             this.Lewo.TabIndex = 3;
-            this.Lewo.Text = "<-";
+            this.Lewo.Text = "←";
             this.Lewo.UseVisualStyleBackColor = true;
             // 
             // pictureBox1
             // 
             this.pictureBox1.Location = new System.Drawing.Point(270, 27);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(517, 363);
+            this.pictureBox1.Size = new System.Drawing.Size(518, 358);
             this.pictureBox1.TabIndex = 4;
             this.pictureBox1.TabStop = false;
             // 
@@ -110,20 +119,20 @@
             // dodajToolStripMenuItem
             // 
             this.dodajToolStripMenuItem.Name = "dodajToolStripMenuItem";
-            this.dodajToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.dodajToolStripMenuItem.Size = new System.Drawing.Size(117, 22);
             this.dodajToolStripMenuItem.Text = "Dodaj";
             this.dodajToolStripMenuItem.Click += new System.EventHandler(this.dodajToolStripMenuItem_Click);
             // 
             // ususnToolStripMenuItem
             // 
             this.ususnToolStripMenuItem.Name = "ususnToolStripMenuItem";
-            this.ususnToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.ususnToolStripMenuItem.Size = new System.Drawing.Size(117, 22);
             this.ususnToolStripMenuItem.Text = "Ususn";
             // 
             // zamknijToolStripMenuItem
             // 
             this.zamknijToolStripMenuItem.Name = "zamknijToolStripMenuItem";
-            this.zamknijToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.zamknijToolStripMenuItem.Size = new System.Drawing.Size(117, 22);
             this.zamknijToolStripMenuItem.Text = "Zamknij";
             // 
             // listView1
@@ -132,15 +141,88 @@
             this.listView1.HideSelection = false;
             this.listView1.Location = new System.Drawing.Point(13, 28);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(251, 410);
+            this.listView1.Size = new System.Drawing.Size(251, 493);
             this.listView1.TabIndex = 6;
             this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // Góra
+            // 
+            this.Góra.Location = new System.Drawing.Point(316, 391);
+            this.Góra.Name = "Góra";
+            this.Góra.Size = new System.Drawing.Size(40, 40);
+            this.Góra.TabIndex = 7;
+            this.Góra.Text = "↑";
+            this.Góra.UseVisualStyleBackColor = true;
+            // 
+            // Dół
+            // 
+            this.Dół.Location = new System.Drawing.Point(316, 481);
+            this.Dół.Name = "Dół";
+            this.Dół.Size = new System.Drawing.Size(40, 40);
+            this.Dół.TabIndex = 8;
+            this.Dół.Text = "↓";
+            this.Dół.UseVisualStyleBackColor = true;
+            // 
+            // Reset
+            // 
+            this.Reset.Location = new System.Drawing.Point(316, 437);
+            this.Reset.Name = "Reset";
+            this.Reset.Size = new System.Drawing.Size(40, 40);
+            this.Reset.TabIndex = 9;
+            this.Reset.Text = "∰";
+            this.Reset.UseVisualStyleBackColor = true;
+            this.Reset.Click += new System.EventHandler(this.Reset_Click);
+            // 
+            // Wczytaj
+            // 
+            this.Wczytaj.Location = new System.Drawing.Point(474, 400);
+            this.Wczytaj.Name = "Wczytaj";
+            this.Wczytaj.Size = new System.Drawing.Size(75, 23);
+            this.Wczytaj.TabIndex = 10;
+            this.Wczytaj.Text = "Load";
+            this.Wczytaj.UseVisualStyleBackColor = true;
+            this.Wczytaj.Click += new System.EventHandler(this.Wczytaj_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(474, 429);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "Clear";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // RotPraw
+            // 
+            this.RotPraw.Location = new System.Drawing.Point(362, 391);
+            this.RotPraw.Name = "RotPraw";
+            this.RotPraw.Size = new System.Drawing.Size(40, 40);
+            this.RotPraw.TabIndex = 14;
+            this.RotPraw.Text = "↷";
+            this.RotPraw.UseVisualStyleBackColor = true;
+            // 
+            // RotLew
+            // 
+            this.RotLew.Location = new System.Drawing.Point(270, 391);
+            this.RotLew.Name = "RotLew";
+            this.RotLew.Size = new System.Drawing.Size(40, 40);
+            this.RotLew.TabIndex = 15;
+            this.RotLew.Text = "↶";
+            this.RotLew.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 533);
+            this.Controls.Add(this.RotLew);
+            this.Controls.Add(this.RotPraw);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.Wczytaj);
+            this.Controls.Add(this.Reset);
+            this.Controls.Add(this.Dół);
+            this.Controls.Add(this.Góra);
             this.Controls.Add(this.listView1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.Lewo);
@@ -172,6 +254,13 @@
         private System.Windows.Forms.ToolStripMenuItem ususnToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem zamknijToolStripMenuItem;
         private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button Góra;
+        private System.Windows.Forms.Button Dół;
+        private System.Windows.Forms.Button Reset;
+        private System.Windows.Forms.Button Wczytaj;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button RotPraw;
+        private System.Windows.Forms.Button RotLew;
     }
 }
 
--- Zad2/Form1.cs
+++ Zad2/Form1.cs
@@ -7,15 +7,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.IO;
+using System.Windows.Input;
+using Microsoft.Win32;
+using System.Windows;
 namespace Zad2
 {
     public partial class Form1 : Form
     {
+        int i = 0;
+        int j = 0;
         Image image;
         double zoom;
         List<String> Ścieszka = new List<String>();
         ImageList Obraz = new ImageList();
+       
+
 
 
 
@@ -23,44 +30,101 @@ namespace Zad2
         {
             InitializeComponent();
             listView1.View = View.Details;
-            listView1.Columns.Add("Obrazy", 200);
-            listView1.AutoResizeColumn(0, ColumnHeaderAutoResizeStyle.HeaderSize);
+            listView1.Columns.Add("Obrazy", -2);
             Obraz.ImageSize = new Size(100, 100);
+            listView1.SmallImageList = Obraz;
+            listView1.MultiSelect = false;
+            
+            
+            
         }
 
         private void dodajToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            int i = 0;
-            int j = 0;
-            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            try
             {
-                openFileDialog.InitialDirectory = "c:\\";
-                openFileDialog.Filter = " Images (*.PNG,*.BMP,*.JPG,*.GIF)|*.png;*.BMP;*.JPG;*.GIF|All files(*.*)|*.*";
-                openFileDialog.RestoreDirectory = true;
-                try {
-                    foreach(String file in openFileDialog.FileNames)
+                listView1.Items.Clear();
+                Obraz.Images.Clear();
+                Ścieszka.Clear();
+                OpenFileDialog openFileDialog = new OpenFileDialog{
+                InitialDirectory = "C:\\Users\\Modzel\\Documents\\My Games\\Fallout 76\\Photos\\6f24a1582fa547048f7bdcd7a5adf5b1",
+                Filter = " Images (*.PNG,*.BMP,*.JPG,*.GIF)|*.png;*.BMP;*.JPG;*.GIF|All files(*.*)|*.*",
+                RestoreDirectory = true,
+                Multiselect = true,
+                };
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK){
+                    foreach (String file in openFileDialog.FileNames)
                     {
                         try
                         {
-                                Ścieszka.Add(file);
+                            Ścieszka.Add(file);
                         }
-                        catch (Exception ex) { MessageBox.Show("ERROR", ex.ToString()); }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show(ex.ToString(),"ERROR"); 
+                        }
                     }
-                    listView1.SmallImageList = Obraz;
-                    for (j = 0; j < Ścieszka.Count; i++)
+                    
+                    for (j = 0; j < Ścieszka.Count; j++)
                     {
                         Obraz.Images.Add(Image.FromFile(Ścieszka[j]));
                     }
-                    for(i = 0; i < Ścieszka.Count; i++)
+                    for (i = 0; i < Ścieszka.Count; i++)
                     {
-                        
+                        listView1.Items.Add(Ścieszka[i], i);
                     }
+                    
+                    
                 }
-                catch(Exception ex) {
-                    MessageBox.Show("ERROR",ex.ToString());
-                }
-    
+
             }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "ERROR");
+            }
+        }
+
+        private void Wczytaj_Click(object sender, EventArgs e)
+        {
+            String text = listView1.SelectedItems[0].Text;
+            pictureBox1.ImageLocation = text.ToString();
+            
+        }
+        private void pictureBox_MouseWheel(object sender, MouseEventArgs e)
+        {
+            if(e.Delta != 0)
+            {
+                if (e.Delta > 0)
+                {
+                    
+                }
+                else if(e.Delta < 0)
+                {
+                
+                }
+            }
+        }
+
+        private void Plus_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Minus_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            pictureBox1.ImageLocation = "";
+        }
+
+        private void Reset_Click(object sender, EventArgs e)
+        {
+            pictureBox1.SizeMode = PictureBoxSizeMode.CenterImage;
+
         }
     }
 }
