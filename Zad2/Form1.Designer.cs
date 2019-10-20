c4f34184d21b5962d3585c97796ae8dabc0aeaef
Modzel <hootwheels989@gmail.com>
2019-10-20 16:50:16 +0200

    Zrobione tak w 70 %

--- Zad2/Form1.Designer.cs
+++ Zad2/Form1.Designer.cs
@@ -46,15 +46,17 @@
             this.button5 = new System.Windows.Forms.Button();
             this.RotPraw = new System.Windows.Forms.Button();
             this.RotLew = new System.Windows.Forms.Button();
+            this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
+            this.vScrollBar1 = new System.Windows.Forms.VScrollBar();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // Plus
             // 
-            this.Plus.Location = new System.Drawing.Point(700, 436);
+            this.Plus.Location = new System.Drawing.Point(270, 251);
             this.Plus.Name = "Plus";
-            this.Plus.Size = new System.Drawing.Size(75, 42);
+            this.Plus.Size = new System.Drawing.Size(132, 42);
             this.Plus.TabIndex = 0;
             this.Plus.Text = "+";
             this.Plus.UseVisualStyleBackColor = true;
@@ -62,9 +64,9 @@
             // 
             // Minus
             // 
-            this.Minus.Location = new System.Drawing.Point(619, 436);
+            this.Minus.Location = new System.Drawing.Point(270, 299);
             this.Minus.Name = "Minus";
-            this.Minus.Size = new System.Drawing.Size(75, 42);
+            this.Minus.Size = new System.Drawing.Size(132, 42);
             this.Minus.TabIndex = 1;
             this.Minus.Text = "-";
             this.Minus.UseVisualStyleBackColor = true;
@@ -72,16 +74,17 @@
             // 
             // Prawo
             // 
-            this.Prawo.Location = new System.Drawing.Point(362, 437);
+            this.Prawo.Location = new System.Drawing.Point(362, 74);
             this.Prawo.Name = "Prawo";
             this.Prawo.Size = new System.Drawing.Size(40, 40);
             this.Prawo.TabIndex = 2;
             this.Prawo.Text = "→";
             this.Prawo.UseVisualStyleBackColor = true;
+            this.Prawo.Click += new System.EventHandler(this.Prawo_Click_1);
             // 
             // Lewo
             // 
-            this.Lewo.Location = new System.Drawing.Point(270, 437);
+            this.Lewo.Location = new System.Drawing.Point(270, 74);
             this.Lewo.Name = "Lewo";
             this.Lewo.Size = new System.Drawing.Size(40, 40);
             this.Lewo.TabIndex = 3;
@@ -90,11 +93,15 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(270, 27);
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(408, 27);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(518, 358);
+            this.pictureBox1.Size = new System.Drawing.Size(773, 474);
             this.pictureBox1.TabIndex = 4;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint_1);
             // 
             // menuStrip1
             // 
@@ -102,7 +109,7 @@
             this.opcjieToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(1218, 24);
             this.menuStrip1.TabIndex = 5;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -128,15 +135,19 @@
             this.ususnToolStripMenuItem.Name = "ususnToolStripMenuItem";
             this.ususnToolStripMenuItem.Size = new System.Drawing.Size(117, 22);
             this.ususnToolStripMenuItem.Text = "Ususn";
+            this.ususnToolStripMenuItem.Click += new System.EventHandler(this.ususnToolStripMenuItem_Click);
             // 
             // zamknijToolStripMenuItem
             // 
             this.zamknijToolStripMenuItem.Name = "zamknijToolStripMenuItem";
             this.zamknijToolStripMenuItem.Size = new System.Drawing.Size(117, 22);
             this.zamknijToolStripMenuItem.Text = "Zamknij";
+            this.zamknijToolStripMenuItem.Click += new System.EventHandler(this.zamknijToolStripMenuItem_Click);
             // 
             // listView1
             // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
             this.listView1.BackColor = System.Drawing.SystemColors.ScrollBar;
             this.listView1.HideSelection = false;
             this.listView1.Location = new System.Drawing.Point(13, 28);
@@ -147,7 +158,7 @@
             // 
             // Góra
             // 
-            this.Góra.Location = new System.Drawing.Point(316, 391);
+            this.Góra.Location = new System.Drawing.Point(316, 28);
             this.Góra.Name = "Góra";
             this.Góra.Size = new System.Drawing.Size(40, 40);
             this.Góra.TabIndex = 7;
@@ -156,7 +167,7 @@
             // 
             // Dół
             // 
-            this.Dół.Location = new System.Drawing.Point(316, 481);
+            this.Dół.Location = new System.Drawing.Point(316, 118);
             this.Dół.Name = "Dół";
             this.Dół.Size = new System.Drawing.Size(40, 40);
             this.Dół.TabIndex = 8;
@@ -165,7 +176,7 @@
             // 
             // Reset
             // 
-            this.Reset.Location = new System.Drawing.Point(316, 437);
+            this.Reset.Location = new System.Drawing.Point(316, 74);
             this.Reset.Name = "Reset";
             this.Reset.Size = new System.Drawing.Size(40, 40);
             this.Reset.TabIndex = 9;
@@ -175,9 +186,9 @@
             // 
             // Wczytaj
             // 
-            this.Wczytaj.Location = new System.Drawing.Point(474, 400);
+            this.Wczytaj.Location = new System.Drawing.Point(270, 187);
             this.Wczytaj.Name = "Wczytaj";
-            this.Wczytaj.Size = new System.Drawing.Size(75, 23);
+            this.Wczytaj.Size = new System.Drawing.Size(132, 23);
             this.Wczytaj.TabIndex = 10;
             this.Wczytaj.Text = "Load";
             this.Wczytaj.UseVisualStyleBackColor = true;
@@ -185,9 +196,9 @@
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(474, 429);
+            this.button5.Location = new System.Drawing.Point(270, 216);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.Size = new System.Drawing.Size(132, 23);
             this.button5.TabIndex = 11;
             this.button5.Text = "Clear";
             this.button5.UseVisualStyleBackColor = true;
@@ -195,27 +206,52 @@
             // 
             // RotPraw
             // 
-            this.RotPraw.Location = new System.Drawing.Point(362, 391);
+            this.RotPraw.Location = new System.Drawing.Point(362, 28);
             this.RotPraw.Name = "RotPraw";
             this.RotPraw.Size = new System.Drawing.Size(40, 40);
             this.RotPraw.TabIndex = 14;
             this.RotPraw.Text = "↷";
             this.RotPraw.UseVisualStyleBackColor = true;
+            this.RotPraw.Click += new System.EventHandler(this.RotPraw_Click);
             // 
             // RotLew
             // 
-            this.RotLew.Location = new System.Drawing.Point(270, 391);
+            this.RotLew.Location = new System.Drawing.Point(270, 28);
             this.RotLew.Name = "RotLew";
             this.RotLew.Size = new System.Drawing.Size(40, 40);
             this.RotLew.TabIndex = 15;
             this.RotLew.Text = "↶";
             this.RotLew.UseVisualStyleBackColor = true;
+            this.RotLew.Click += new System.EventHandler(this.RotLew_Click);
+            // 
+            // hScrollBar1
+            // 
+            this.hScrollBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.hScrollBar1.Location = new System.Drawing.Point(408, 504);
+            this.hScrollBar1.Name = "hScrollBar1";
+            this.hScrollBar1.Size = new System.Drawing.Size(773, 17);
+            this.hScrollBar1.TabIndex = 16;
+            this.hScrollBar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.hScrollBar1_Scroll_1);
+            // 
+            // vScrollBar1
+            // 
+            this.vScrollBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.vScrollBar1.Location = new System.Drawing.Point(1188, 28);
+            this.vScrollBar1.Name = "vScrollBar1";
+            this.vScrollBar1.Size = new System.Drawing.Size(17, 473);
+            this.vScrollBar1.TabIndex = 17;
+            this.vScrollBar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.vScrollBar1_Scroll_1);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 533);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(1218, 533);
+            this.Controls.Add(this.vScrollBar1);
+            this.Controls.Add(this.hScrollBar1);
             this.Controls.Add(this.RotLew);
             this.Controls.Add(this.RotPraw);
             this.Controls.Add(this.button5);
@@ -261,6 +297,8 @@
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Button RotPraw;
         private System.Windows.Forms.Button RotLew;
+        private System.Windows.Forms.HScrollBar hScrollBar1;
+        private System.Windows.Forms.VScrollBar vScrollBar1;
     }
 }
 
--- Zad2/Form1.cs
+++ Zad2/Form1.cs
@@ -18,11 +18,13 @@ namespace Zad2
         int i = 0;
         int j = 0;
         Image image;
-        double zoom;
+        double zoom = 1;
+        Bitmap bitmap;
+        bool rot = false;
         List<String> Ścieszka = new List<String>();
         ImageList Obraz = new ImageList();
        
-
+        
 
 
 
@@ -34,10 +36,15 @@ namespace Zad2
             Obraz.ImageSize = new Size(100, 100);
             listView1.SmallImageList = Obraz;
             listView1.MultiSelect = false;
-            
-            
-            
+            pictureBox1.MouseWheel += new MouseEventHandler(pictureBox_MouseWheel);
+            hScrollBar1.Width = pictureBox1.Width;
+            hScrollBar1.Left = pictureBox1.Left;
+            hScrollBar1.Top = pictureBox1.Bottom;
+            vScrollBar1.Height = pictureBox1.Height;
+            vScrollBar1.Left = pictureBox1.Left + pictureBox1.Width;
+            vScrollBar1.Top = pictureBox1.Top;
         }
+
 
         private void dodajToolStripMenuItem_Click(object sender, EventArgs e)
         {
@@ -46,11 +53,15 @@ namespace Zad2
                 listView1.Items.Clear();
                 Obraz.Images.Clear();
                 Ścieszka.Clear();
-                OpenFileDialog openFileDialog = new OpenFileDialog{
-                InitialDirectory = "C:\\Users\\Modzel\\Documents\\My Games\\Fallout 76\\Photos\\6f24a1582fa547048f7bdcd7a5adf5b1",
-                Filter = " Images (*.PNG,*.BMP,*.JPG,*.GIF)|*.png;*.BMP;*.JPG;*.GIF|All files(*.*)|*.*",
-                RestoreDirectory = true,
-                Multiselect = true,
+                OpenFileDialog openFileDialog = new OpenFileDialog {
+                    InitialDirectory = "C:\\Users\\Modzel\\Documents\\My Games\\Fallout 76\\Photos\\6f24a1582fa547048f7bdcd7a5adf5b1",
+                    Filter = " Images (*.PNG,*.BMP,*.JPG,*.GIF)|*.png;*.BMP;*.JPG;*.GIF|All files(*.*)|*.*",
+                    RestoreDirectory = true,
+                    Multiselect = true,
+                    ValidateNames = false,
+                    CheckFileExists = false,
+                    CheckPathExists = true
+                    
                 };
 
                 if (openFileDialog.ShowDialog() == DialogResult.OK){
@@ -87,8 +98,18 @@ namespace Zad2
 
         private void Wczytaj_Click(object sender, EventArgs e)
         {
-            String text = listView1.SelectedItems[0].Text;
-            pictureBox1.ImageLocation = text.ToString();
+            try
+            {
+                String text = listView1.SelectedItems[0].Text;
+                pictureBox1.ImageLocation = text.ToString();
+                bitmap = new Bitmap(Image.FromFile(listView1.SelectedItems[0].Text));
+                image = Image.FromFile(listView1.SelectedItems[0].Text);
+            }
+            catch
+            {
+                MessageBox.Show("Wybierz obraz", "Error");
+            }
+
             
         }
         private void pictureBox_MouseWheel(object sender, MouseEventArgs e)
@@ -97,28 +118,55 @@ namespace Zad2
             {
                 if (e.Delta > 0)
                 {
-                    
+                    Plus_Click(sender,e);
                 }
                 else if(e.Delta < 0)
                 {
-                
+                    Minus_Click(sender,e);
                 }
             }
         }
 
         private void Plus_Click(object sender, EventArgs e)
         {
+            try
+            {
 
+                if (zoom < 5) zoom = zoom + 0.1;
+                Bitmap bmp = new Bitmap(image, Convert.ToInt32(image.Width * zoom), Convert.ToInt32(image.Height * zoom));
+                Graphics g = Graphics.FromImage(bmp);
+                g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+                pictureBox1.Focus();
+                bitmap = bmp;
+                pictureBox1.Image = bmp;
+                pictureBox1.Refresh();
+                
+            }
+            catch { }
         }
 
         private void Minus_Click(object sender, EventArgs e)
         {
-
+            try
+            {
+                if (zoom > 0.5) zoom = zoom - 0.1;
+                Bitmap bmp = new Bitmap(image, Convert.ToInt32(image.Width * zoom), Convert.ToInt32(image.Height* zoom));
+                Graphics g = Graphics.FromImage(bmp);
+                g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+                pictureBox1.Focus();
+                bitmap = bmp;
+                pictureBox1.Image = bmp;
+                pictureBox1.Refresh();
+               
+                
+            }
+            catch { }
         }
 
         private void button5_Click(object sender, EventArgs e)
         {
             pictureBox1.ImageLocation = "";
+            pictureBox1.Refresh();
         }
 
         private void Reset_Click(object sender, EventArgs e)
@@ -126,5 +174,109 @@ namespace Zad2
             pictureBox1.SizeMode = PictureBoxSizeMode.CenterImage;
 
         }
+
+
+        private void RotPraw_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (rot == true)
+                {
+                    
+                    bitmap.RotateFlip(RotateFlipType.Rotate90FlipX);
+                    pictureBox1.Image = bitmap;
+                    image = bitmap;
+                    rot = false;
+                    pictureBox1.Refresh();
+                }
+                else
+                {
+                    bitmap.RotateFlip(RotateFlipType.Rotate90FlipY);
+                    pictureBox1.Image = bitmap;
+                    image = bitmap;
+                    rot = true;
+                    pictureBox1.Refresh();
+                }
+
+            }
+            catch { }
+
+        }
+
+        private void RotLew_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (rot == false)
+                {
+                    bitmap.RotateFlip(RotateFlipType.Rotate90FlipX);
+                    pictureBox1.Image = bitmap;
+                    image = bitmap;
+                    rot = true;
+                    pictureBox1.Refresh();
+
+                }
+                else
+                {
+                    bitmap.RotateFlip(RotateFlipType.Rotate90FlipY);
+                    pictureBox1.Image = bitmap;
+                    image = bitmap;
+                    rot = false;
+                    pictureBox1.Refresh();
+                }
+            }
+            catch { }
+        }
+
+        private void ususnToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try {
+                listView1.Items.Remove(listView1.SelectedItems[0]);
+                }
+            catch
+            {
+                MessageBox.Show("Wybierz Plik", "ERROR");
+            }
+        }
+        private void zamknijToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+
+
+
+        int x = 0;
+        int y = 0;
+        private void hScrollBar1_Scroll_1(object sender, ScrollEventArgs e)
+        {
+            x = (sender as HScrollBar).Value *10;
+            pictureBox1.Refresh();
+        }
+
+        private void vScrollBar1_Scroll_1(object sender, ScrollEventArgs e)
+        {
+            y = (sender as VScrollBar).Value *10;
+            pictureBox1.Refresh();
+        }
+        private void Prawo_Click_1(object sender, EventArgs e)
+        {
+            y++;
+        }
+        private void pictureBox1_Paint_1(object sender, PaintEventArgs e)
+        {
+            try {
+                
+                pictureBox1 = sender as PictureBox;
+                e.Graphics.DrawImage(pictureBox1.Image, e.ClipRectangle, x, y, e.ClipRectangle.Width, e.ClipRectangle.Height, GraphicsUnit.Pixel);
+            }
+            catch
+            {
+
+            }
+        }
+
+
+ 
     }
 }
